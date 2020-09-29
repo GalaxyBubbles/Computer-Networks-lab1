6de2eec34c1d7da8c6e809b329d0156da2772325
宋凝儿 <42276208+SongNingEr@users.noreply.github.com>
2020-09-29 20:36:40 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,182 +1,134 @@
-#include "pch.h"
-#include <queue>
-#include <ctime>
-#include <math.h>
-#include <iostream>
-#include <Windows.h>
+// EE 465, USC
+// Simulation of an M/M/1 system
 
-#define BUSY 1
-#define FREE 0
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
 
 using namespace std;
-clock_t _time = clock();
-/*
-Assignment 1£º
 
-Multi-queue simulation
-	  Description: a transmission system subject to 3 inputs and only one server. All inputs are characterized by Poisson process and the service time follows an exponential distribution.
+#define INFIN  			999999999
 
-	  Goal: explore a scheduling scheme which is able to allocate the service fairly to each of the input in terms of any performance measurement; obtaining the mean queue length, mean waiting time, queue length distribution and waiting time distribution for each queue. Any necessary explaination and statement should be included in the report.
-*/
-
-//generate possion distribution
-int possion(double lambda)
+// Function Name:	expon
+// Description: 	Generates an exponentially distributed random number 
+//					with parameter \lambda. 
+// Input: 			lambda (double)
+// Output: 			An exponentially distributed random number (double)
+//
+double expon(double lambda)
 {
-	int i, x;
-	double a, b, u;
-
+	double u; // used to store a random number. 
 	srand((int)clock());
-	a = exp(-lambda);
-	i = 0;
-	b = 1.0;
-	do {
-		u = rand() / double(RAND_MAX);
-		b *= u;
-		i++;
-	} while (b >= a);
-	x = i - 1;
-	return x;
+	do
+	{
+		u = rand() % 10000 / 10000.0; //uniform number in the interval [0.0, 1.0]
+	} while ((u == 0) || (u == 1)); //special cases that we want to avoid
+
+	return -log(1 - u) / lambda;
+
 }
 
-double exponent(double lambda)
+// Function Name:	print_stats
+// Description: 	Saves and prints system statistics 
+// Input:           stats_file (ostream object): ostream object for the stats file
+//					avg_customers (double): average customers in the system
+//					avg_service_time (double): average service time 
+// Output: 			void (output stored to file and printed in the screen)
+//
+void print_stats(ostream &stats_file, double* wait_time,int tot_arrival)
 {
-	srand((int)clock());
-	double pV = 0.0;
-	while (true)
+	for (int i = 0; i < tot_arrival; i++)
 	{
-		pV = (double)rand() / (double)RAND_MAX;
-		if (pV != 1)
-		{
-			break;
-		}
-	}
-	pV = (-10000.0 / lambda)*log(1 - pV);
-	return pV;
-}
-
-
-class packet
-{
-public:
-	packet()
-	{
-		itv_time = (int)exponent(lambda);
-		Sleep(itv_time);
-		arv_time = clock();
-		wat_time = (int)exponent(beta);
-	}
-	double get_arv_time() { return arv_time; }
-	double get_wat_time() { return wat_time; }
-	double get_sev_time() { return sev_time; }
-	double get_itv_time() { return itv_time; }
-	void set_sev_time(int t) { sev_time = t; }
-private:
-	double arv_time;
-	double itv_time;//interval of arrive time
-	double sev_time;//the time a packet get service
-	double wat_time;//total serve time
-	int lambda = 4;
-	int beta = 8;
-};
-
-class server
-{
-public:
-	bool free() { return state; }
-	void serve(packet *p,int count)
-	{ 
-		pac = p; 
-		p->set_sev_time(_time);
-		cout << count << "is served now  " << p->get_sev_time() << "  wait time:  " << p->get_wat_time() << endl;
-	}
-	void change()
-	{
-		if (state == BUSY) pac = NULL;
-		state = !state;
-	}
-	void check(queue<packet> &Q, int count)
-	{
-		if (pac == NULL) return; 
-		_time = clock();
-		if (_time - pac->get_sev_time() >= pac->get_wat_time() && state == BUSY)
-		{//end service
-			state = FREE;
-			cout << count << "end serve  "<<endl;
-		}
-		if (state == FREE && !Q.empty())
-		{//continue serve new packet
-			packet p = Q.front();
-			Q.pop();
-			serve(&p,count);
-		}
-	}
-	void set_pac(packet *p) { pac = p; }
-	bool get_state() { return state; }
-private:
-	bool state;//busy:1 free:0
-	packet *pac;
-};
-
-void Arrive(queue<packet> &Q, server &s,int count)
-{
-	packet p;
-	if (!Q.empty())
-	{
-		Q.push(p);
-		cout << count << "  enter queue"
-			<< "  arrive time:  " << p.get_arv_time()
-			<< "  interval time:  " << p.get_itv_time()
-			<< endl;
-		cout << "Queue size: " << Q.size() << endl;
-	}
-	else
-	{
-		if (s.free())
-		{
-			s.serve(&p,count);
-			cout << count << "  on serve  "<<endl;
-			s.change();//server change state
-		}
-		else
-		{
-			Q.push(p);
-			cout << count << "  enter queue(queue empty and server busy)  " 
-				<<"arrive time:  "<< p.get_arv_time() 
-				<< "  interval time:" << p.get_itv_time() 
-				<< endl;
-			cout << "Queue size: " << Q.size() << endl;
-
-		}
+		stats_file << wait_time[i] << endl;
 	}
 }
 
-int main()
-{
-	int pac_num = 10;//packet number
-	server s;
-	queue<packet> Q;
-	int count = 0;
+// The main function of the program that is called at the beginning.
+int main() {
+	//system variables
+	long int tot_arrivals, cur_arrivals = 0;
+	double lambda, mu;
+	double event1 = 0.0, event2 = INFIN; //event1: time of next arrival, event2: time of next departure
+	double service_time, cur_time = 0.0;
+	int customers = 0;
+	double avg_customers = 0.0, avg_service_time = 0.0; //used for calculating statistics
+	ofstream stats_file; //file handler for saving the statistics in a file
 
-	Arrive(Q, s, count);//renew the packet data
+	//get input from the user
+	cout << "Basic M/M/1 Simulator" << endl;
+	cout << "Enter the total number of arrivals: ";
+	cin >> tot_arrivals;
+	cout << "Enter lambda: ";
+	cin >> lambda;
+	cout << "Enter mu: ";
+	cin >> mu;
 
-	while (!s.free() || !Q.empty())
+	stats_file.open("stats.txt"); //it creates the file, if it does not exist
+
+	double *wait_time = new double[tot_arrivals];
+	double *arv_time = new double[tot_arrivals];
+
+	for (int i = 0; i < tot_arrivals; i++)
 	{
-		count++;
-		if (count < 10)
-		{
-			Arrive(Q, s, count);
-			s.check(Q, count);
-		}		
-
-		s.check(Q, count);
-		//cout << "s state: " << s.get_state() << endl;
-		//cout << Q.size() << endl;
+		wait_time[i] = 0;
+		arv_time[i] = 0;
 	}
 
+	int cur_serve = 0;
 
-	//Depart(&s);
-	//clear a packet from queue
-	
-	getchar();
+	// discrete event simulator 
+	while (cur_arrivals < tot_arrivals)
+	{
+		cout << "cur_arrivals: " << cur_arrivals << endl;
+		if (event1 < event2)	//arrival
+		{
+			cur_arrivals++;
+			avg_customers += customers;
+			customers++;
+			cur_time = event1;
+			arv_time[cur_arrivals] = event1;
+			event1 = cur_time + expon(lambda);
+			if (customers == 1)	//if there is only 1 customer, he/she goes directly to service
+			{
+				cur_serve = cur_arrivals;
+				service_time = expon(mu);
+				wait_time[cur_arrivals] = service_time;
+				cout << cur_serve << "  " << wait_time[cur_arrivals] << endl;
+				event2 = cur_time + service_time;
+				avg_service_time += service_time;
+			}
+		}
+		else					//departure
+		{
+			customers--;
+			cur_time = event2;//server free
+			if (customers > 0) // the departure left a non-empty queue behind
+			{
+				cur_serve++;
+				service_time = expon(mu);
+				event2 = cur_time + service_time;
+				avg_service_time += service_time;
+				wait_time[cur_serve] = cur_time - arv_time[cur_serve];
+				cout << cur_serve << "  " << wait_time[cur_arrivals] << endl;
+			}
+			else 				//the departure left the queue empty
+			{
+				event2 = INFIN; //no departure scheduled
+			}
+		}
+	}
+	avg_customers /= cur_arrivals;
+	avg_service_time /= (cur_arrivals - customers);
+	//print_stats(stats_file, avg_customers, avg_service_time);
+	//stats_file.close();
+	for (int i = 1; i < tot_arrivals; i++)
+		cout << i << " wait time: " << wait_time[i] << endl;
+	print_stats(stats_file, wait_time, tot_arrivals);
+	stats_file.close();
+	int a;
+	cin >> a;
 	return 0;
 }
